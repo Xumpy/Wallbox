af2f80e3258f556fe920da2fcdc45bd81f45f6e6
Martens <nico.martens@apg.nl>
2021-05-26 11:51:48 +0200

    Adding caching mechanisme for multiple instant calls

--- src/main/java/com/xumpy/wallbox/WallboxController.java
+++ src/main/java/com/xumpy/wallbox/WallboxController.java
@@ -2,6 +2,7 @@ package com.xumpy.wallbox;
 
 import com.xumpy.wallbox.module.RestWallbox;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class WallboxController {
     @Autowired RestWallbox restWallbox;
 
     @GetMapping("/start/{chargerId}")
+    @Cacheable("start")
     public String startCharging(@PathVariable Integer chargerId){
         restWallbox.refreshAuthenticateToken();
         restWallbox.startCharging(chargerId);
@@ -19,6 +21,7 @@ public class WallboxController {
     }
 
     @GetMapping("/stop/{chargerId}")
+    @Cacheable("stop")
     public String stopCharging(@PathVariable Integer chargerId){
         restWallbox.refreshAuthenticateToken();
         restWallbox.stopCharging(chargerId);
